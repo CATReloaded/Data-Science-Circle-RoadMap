638361427810dcb8a5d23d947e2a85ca00410274
Nouran Ali <nouranalimohammed@gmail.com>
2020-07-27 16:33:01 +0200

    Create betweeness_cent.py

---
+++ betweeness_cent.py
@@ -0,0 +1,22 @@
+import networkx as nx
+# Define find_node_with_highest_bet_cent()
+def find_node_with_highest_bet_cent(G):
+
+    # Compute betweenness centrality: bet_cent
+    bet_cent = nx.betweenness_centrality(G)
+
+    # Compute maximum betweenness centrality: max_bc
+    max_bc = max(list(bet_cent.values()))
+
+    nodes = set()
+
+    # Iterate over the betweenness centrality dictionary
+    for k, v in bet_cent.items():
+
+        # Check if the current value has the maximum betweenness centrality
+        if v == max_bc:
+
+            # Add the current node to the set of nodes
+            nodes.add(k)
+
+    return nodes
